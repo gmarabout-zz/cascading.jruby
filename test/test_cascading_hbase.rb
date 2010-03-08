7ff4f5c1301594b0412a761261ab5d5434774d89
mwalker <mwalker@etsy.com>
2010-03-08 16:14:58 -0600

    Only run the HBase test if it is installed

--- test/test_cascading_hbase.rb
+++ test/test_cascading_hbase.rb
@@ -4,6 +4,9 @@ require 'cascading-ext/hbase'
 
 class TC_Cascading < Test::Unit::TestCase
   def test_hbase_tap
+    # Don't run this test if the extension isn't installed
+    return unless Cascading::HBASE_HOME && Cascading::CASCADING_HBASE_HOME
+
     tap = Cascading.hbase_tap("my_table", :key=>["key"], :families=>["data", "metaData"], :values=>["val1", "val2"])
     assert tap.get_scheme().is_a? Java::CascadingHbase::HBaseScheme
     assert tap.is_a? Java::CascadingHbase::HBaseTap
