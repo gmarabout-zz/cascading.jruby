a921dc3e70b949622e653cd6cc89a2670b219d41
mwalker <mwalker@etsy.com>
2010-03-09 11:22:14 -0600

    Fixed join sample

--- samples/join.rb
+++ samples/join.rb
@@ -1,34 +1,35 @@
-require "cascading"
+#! /usr/bin/env jruby
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
-input1 = "samples/data/data_join1.txt"
-input2 = "samples/data/data_join2.txt"
-input3 = "samples/data/data_join3.txt"
+require 'cascading'
+require 'samples/cascading'
 
-output = "output/joined"
+input1 = 'samples/data/data_join1.txt'
+input2 = 'samples/data/data_join2.txt'
+input3 = 'samples/data/data_join3.txt'
+output = 'output/joined'
 
-flow = Cascading::Flow.new("Join sample") do
-  source "extract1", tap(input1)
-  source "extract2", tap(input2)
-  source "extract3", tap(input3)
+Cascading::Flow.new('join') do
+  source 'extract1', tap(input1)
+  source 'extract2', tap(input2)
+  source 'extract3', tap(input3)
 
-  sink "join", tap(output, :replace=>true)
-
-  assembly "extract1" do
-    split "line", :pattern => /[.,]*\s+/, :into=> ["id", "name"], :output => ["id", "name"]
+  assembly 'extract1' do
+    split 'line', ['id', 'name']
   end
 
-  assembly "extract2" do
-    split "line", :pattern => /[.,]*\s+/, :into=> ["id", "age"], :output => ["id", "age"]
+  assembly 'extract2' do
+    split 'line', ['id', 'age']
   end
 
-  assembly "extract3" do
-    split "line", :pattern => /[.,]*\s+/, :into=> ["id", "city"], :output => ["id", "city"]
+  assembly 'extract3' do
+    split 'line', ['id', 'city']
   end
-  
-  assembly "join" do
-    join "extract1", "extract2", "extract3", :on => ["id"], :declared_fields => ["id", "name", "id2", "age", "id3", "city"]
-    restrict_to "id", "name", "age", "city"
-  end
-end 
 
-flow.complete
+  assembly 'join' do
+    join 'extract1', 'extract2', 'extract3', :on => 'id'
+    project 'id', 'name', 'age', 'city'
+  end
+
+  sink 'join', tap(output, :sink_mode => :replace)
+end.complete(sample_properties)
