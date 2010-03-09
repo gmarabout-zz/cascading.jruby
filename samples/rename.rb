986e40b81a4f32ebceaeb80d7dcae830468de791
mwalker <mwalker@etsy.com>
2010-03-09 11:54:33 -0600

    Fixed rename sample

--- samples/rename.rb
+++ samples/rename.rb
@@ -1,25 +1,22 @@
-require "cascading"
+#! /usr/bin/env jruby
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'cascading'
+require 'samples/cascading'
 
 input = 'samples/data/data2.txt'
+output = 'output/rename'
 
-output = "output/rename"
+Cascading::Flow.new('rename') do
+  source 'extract', tap(input)
 
-flow = Cascading::Flow.new("rename") do
-  source tap(input)
-  sink tap(output, :replace=>true)
-  
-  assembly "extract" do
-
-    split "line", :pattern => /[.,]*\s+/, :into=>["name", "score1", "score2", "id"], :output => ["name", "score1", "score2", "id"]
-
+  assembly 'extract' do
+    split 'line', ['name', 'score1', 'score2', 'id'], :output => ['name', 'score1', 'score2', 'id']
     assert Java::CascadingOperationAssertion::AssertSizeEquals.new(4)
-
-    rename ["name", "score1", "score2", "id"], ["new_name", "new_score1", "new_score2", "id"]
-   
+    rename 'name' => 'new_name', 'score1' => 'new_score1', 'score2' => 'new_score2'
     assert Java::CascadingOperationAssertion::AssertSizeEquals.new(4)
-   
-    debug :print_fields=>true
+    puts "Final field names: #{scope.values_fields.to_a.inspect}"
   end
-end 
 
-flow.complete
+  sink 'extract', tap(output, :sink_mode => :replace)
+end.complete(sample_properties)
