780dc6ffb12f81250e73f66ff4d054295a8f618e
mwalker <mwalker@etsy.com>
2010-03-10 10:04:53 -0600

    Increment least significant version number to indicate the work we've done, but postpone a release until we've done more testing

--- lib/cascading.rb
+++ lib/cascading.rb
@@ -11,7 +11,7 @@ require 'java'
 module Cascading
 
   # :stopdoc:
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   
